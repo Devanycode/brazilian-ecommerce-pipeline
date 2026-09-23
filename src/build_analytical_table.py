4289db62672c726d28d791c23dbfde2e18aea385
Devanycode <devany.code.gg@gmail.com>
2026-09-23 15:45:54 -0500

    corrección build_analytical_table.py

--- src/build_analytical_table.py
+++ src/build_analytical_table.py
@@ -1,18 +1,26 @@
-import main
+import extract
 import transform as tr
 
-# 1. Extract
-customers_df = main.customers_df
-orders_df = main.orders_df 
-order_items_df = main.order_items_df
-order_products_df = main.order_products_df
-order_payments_df = main.order_payments_df
-sellers_df = main.sellers_df
-order_reviews_df = main.order_reviews_df
+DATA_PATH = "../data"
+
+# 1. EXTRACT
+customers_df = extract.load_customers(f"{DATA_PATH}/olist_customers_dataset.csv")
+orders_df = extract.load_orders(f"{DATA_PATH}/olist_orders_dataset.csv")
+order_items_df = extract.load_order_items(f"{DATA_PATH}/olist_order_items_dataset.csv")
+order_products_df = extract.load_order_products(f"{DATA_PATH}/olist_products_dataset.csv")
+order_payments_df = extract.load_order_payments(f"{DATA_PATH}/olist_order_payments_dataset.csv")
+sellers_df = extract.load_sellers(f"{DATA_PATH}/olist_sellers_dataset.csv")
+order_reviews_df = extract.load_order_reviews(f"{DATA_PATH}/olist_order_reviews_dataset.csv")
 
 
+# 2. TRANSFORM: tablas individuales
+orders_df = tr.conversion_a_datetime(orders_df)
+order_items_df = tr.order_items_shipping_limit_convert_datetime(order_items_df) 
+order_products_df = tr.order_products_convert_float_to_int(order_products_df)
+order_reviews_df = tr.order_reviews_convert_datetime(order_reviews_df)
 
-# 2. Creación de tabla analítica
+
+# 3. Creación de tabla analítica
 # Para evitar problemas de granularidad
 order_reviews_df_modified = tr.preparacion_order_reviews(order_reviews_df)
 tabla = tr.crear_tabla_analitica(
@@ -25,8 +33,7 @@ tabla = tr.crear_tabla_analitica(
     order_reviews_df_modified
 )
 
-#  3. Transformaciones
-tabla = tr.conversion_a_datetime(tabla)
+#  3.1 Transform tabla analítica
 tabla = tr.agregar_total_pedido(tabla)
 tabla = tr.agregar_numero_items(tabla)
 tabla = tr.agregar_porcentaje_item(tabla)
